7c51c612d2ff45a704ffd90989be3ce9c86b594d
idrees538 <mi4003514@gmail.com>
2023-06-09 09:41:03 +0500

    first

--- crud-mern/server/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-const server=express();
-server.use(cors());
-server.use(express.json());
-server.use(express.urlencoded({extended:true}));
